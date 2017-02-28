cde37f520aef608136be4b27e9f5e55a6671ef02
John Dupper <john.dupper@me.com>
2017-02-28 15:03:09 -0500

    testing page load

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,19 @@
 Cypress.config('baseUrl', 'http://localhost:3000')
 
 describe('initial page load', function() {
+
+    beforeEach(function() {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('should load page', function() {
+        cy.request({
+            url: '/movies', followRedirect: false
+        }).then(function(response) {
+            expect(response.status).to.eq(200)
+        })
+    })
+
     it('should have correct title', function () {
         cy.visit('http://localhost:3000')
         cy.title().should('include', 'Movies: Rails')
