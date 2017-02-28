ee27863cdb74848866a5f366a94b9b7ab69a0a04
John Dupper <john.dupper@me.com>
2017-02-28 15:03:26 -0500

    seed two movies to test functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(
+  title: 'Fight Club',
+  year: '1999',
+  rated: 'R',
+  runtime: '139 min',
+  genre: 'Drama',
+  director: 'David Fincher',
+  writer: 'Chuck Palahniuk (novel), Jim Uhls (screenplay)',
+  actors: 'Edward Norton, Brad Pitt, Meat Loaf, Zach Grenier',
+  plot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soap maker, forming an underground fight club that evolves into something much, much more.',
+  awards: 'Nominated for 1 Oscar. Another 10 wins & 31 nominations.',
+  poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1YmU2ZjEtYWIwMC00ZjM3LWI0NTctMDVlNGQ3YmYwMzE5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+rating: '8.8'
+)
 
-Movie.create(title: "Fight Club")
+Movie.create(
+  title: 'La La Land',
+  year: '2016',
+  rated: 'PG-13',
+  runtime: '128 min',
+  genre: 'Comedy, Drama, Musical',
+  director: 'Damien Chazelle',
+  writer: 'Damien Chazelle',
+  actors: 'Ryan Gosling, Emma Stone, Amiée Conn, Terry Walters',
+  plot: 'A jazz pianist falls for an aspiring actress in Los Angeles.',
+  awards: 'Nominated for 14 Oscars. Another 153 wins & 204 nominations.',
+  poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SX300.jpg',
+  rating: '8.5'
+)
