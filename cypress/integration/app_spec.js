4283b9dff1b1464d16eae907a667a680ac72c6af
John Dupper <john.dupper@me.com>
2017-03-01 10:51:19 -0500

    added comment to clarify location test

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -14,6 +14,7 @@ describe('initial page load', function() {
             .then(function(response) {
             expect(response.status).to.eq(302)
         })
+        // without login, should redirect to user path
         cy.location().its('pathname').should('contain', "/users")
     })
 })
