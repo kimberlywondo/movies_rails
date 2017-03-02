d764ca15a7625b79f49f9c755378f5e819a1c2d0
John Dupper <john.dupper@me.com>
2017-03-02 15:57:44 -0500

    added search, results routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
   resources :movies
-  get '/search', to: 'movies#search'
-  get '/results', to: 'movies#results'
+  get  '/search',  to: 'movies#search'
+  post '/results', to: 'movies#results'
+  get  '/results', to: 'movies#results'
 end
