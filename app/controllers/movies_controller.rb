293a862b6e3a6dfd9938da7b4ce3014aefd57c11
John Dupper <john.dupper@me.com>
2017-03-03 16:22:14 -0500

    new, create, index route updated to reflect current_user.id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,27 @@ class MoviesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @movies = Movie.all
+    @movies = Movie.where(user_id: current_user.id)
   end
 
   def show
     @movie = Movie.find_by_id(params[:id])
+  end
+
+
+
+  def new
+    @movie = Movie.new
+  end
+
+
+  def create
+    @movie = Movie.create(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :search
+    end
   end
 
   def edit
@@ -22,8 +38,18 @@ class MoviesController < ApplicationController
   def results
     api_result = HTTParty.get("http://www.omdbapi.com/?t=#{params[:title]}&plot=long&r=json")
     if !api_result["Title"]
-      redirect_to "/search"
+      redirect_to :search
     end
     @movie_results = api_result
+  end
+
+
+
+
+  private
+  def movie_params
+    params.permit(:title, :rated, :released, :runtime, :genre,
+                                  :director, :writer, :actors, :plot, :country,
+                                  :awards, :poster, :metascore, :imdbRating, :user_id)
   end
 end
