4f344f493c3c23e5836062526101dbe5d02e0265
John Dupper <john.dupper@me.com>
2017-03-04 15:21:37 -0500

    mirrored movies, watch controllers for separate CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
   resources :movies
+  resources :watch
 
   get  '/search',  to: 'movies#search'
   post '/results', to: 'movies#results'
   get  '/results', to: 'movies#results'
-
-  get  '/watch',   to: 'watch#index'
-  post '/watch',   to: 'watch#index'
 end
