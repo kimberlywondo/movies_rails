6460745c919650ea87f09fb2482b176856e5a8dd
John Dupper <john.dupper@me.com>
2017-03-04 16:56:02 -0500

    went back to resources from specified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-
   root 'movies#index'
+
   resources :movies
+  get '/movies/remove/:id', to: 'movies#remove'
+
   resources :watch
+  get '/watch/remove/:id', to: 'watch#remove'
 
   get  '/search',  to: 'movies#search'
   post '/results', to: 'movies#results'
   get  '/results', to: 'movies#results'
-
-  get '/movies/remove/:id', to: 'movies#remove'
-  get '/watch/remove/:id', to: 'watch#remove'
-
 end
