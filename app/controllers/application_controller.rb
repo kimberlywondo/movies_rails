a07bbee1255eb7d7055bebc79cef0c91886e1805
John Dupper <john.dupper@me.com>
2017-03-05 20:38:18 -0500

    removed cache settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_filter :set_cache_headers
-
-  private
-
-  def set_cache_headers
-    response.headers["Cache-Control"] = "no-cache, no-store"
-    response.headers["Pragma"] = "no-cache"
-  end
 end
