787ae383c9ed50e46c5491586e950f8edd89276b
John Dupper <john.dupper@me.com>
2017-03-05 20:17:04 -0500

    refactored login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,11 +8,9 @@
 // ***********************************************
 
 
-// failed login attempt
-Cypress.addParentCommand('failLogin', function(email, password) {
-    var email =  email ||'fail@cypress.io'
-    var password = password || 'fail123'
-    var log = Cypress.Log.command({
+Cypress.addParentCommand('login', function(email, password) {
+    var email =  email, password = password,
+        log = Cypress.Log.command({
         name: 'login',
         message: [email, password],
         consoleProps: function() {
@@ -20,33 +18,11 @@ Cypress.addParentCommand('failLogin', function(email, password) {
                 password: password }
         }
     })
-    cy.visit('http://localhost:3000/users/sign_in', { log: false })
-        .contains('Log in', { log: false })
-        .get('#user_email', { log: false }).type(email, { log: false })
-        .get('#user_password', { log: false }).type(password, { log: false })
-        .get('input[type="submit"]', { log: false }).click({ log: false }) // should fail, redirect
-        .url({ log: false}).should('eq','http://localhost:3000/users/sign_in', { log: false })
-        .then(function() { log.snapshot().end() })
-})
 
-
-// successful login
-Cypress.addParentCommand('successLogin', function(email, password) {
-    var email =  email ||'test@cypress.io'
-    var password = password || 'test123'
-    var log = Cypress.Log.command({
-        name: 'login',
-        message: [email, password],
-        consoleProps: function() {
-            return { email: email,
-                  password: password }
-        }
-    })
     cy.visit('http://localhost:3000/users/sign_in', { log: false })
         .contains('Log in', { log: false })
         .get('#user_email', { log: false }).type(email, { log: false })
         .get('#user_password', { log: false }).type(password, { log: false })
         .get('input[type="submit"]', { log: false }).click({ log: false })
-        .url({ log: false}).should('eq','http://localhost:3000/', { log: false })
         .then(function() { log.snapshot().end() })
 })
