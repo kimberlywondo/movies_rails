f9048379323f5846ca6f62b88001d570da6ede71
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-03-11 08:52:22 -0500

    styling

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,13 +9,16 @@ $(document).ready(function(){
     $('.list .list_item').height(highestBox);
 
     // $(window).resize(function() {
-    //    if ($(window).width() > 768) {
-    //       $('.navbar-links').show();
-    //    } else {
-    //       $('.navbar-links').hide();
-    //    }
+    // var highestBox = 0;
+    //     $('.list .list_item').each(function(){
+    //             if($(this).height() > highestBox){
+    //             highestBox = $(this).height();
+    //     }
+    // });
+    // $('.list .list_item').height(highestBox);
     // });
 
 });
+
 
 // TODO fix navbar jquery (window resize)and width
