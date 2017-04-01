bd6da8ba5f62370a82aeac88e0470721f3d88fe9
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-04-01 14:36:49 -0400

    fixed login and signup input field color

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,23 +1,24 @@
-// $(document).on('turbolinks:load', function() {
-//   var highestBox = 0;
-//       $('.list .list_item').each(function(){
-//               if($(this).height() > highestBox){
-//               highestBox = $(this).height();
-//       }
-//   });
-//   $('.list .list_item').height(highestBox);
-// });
-
-$(document).ready(function(){
-
+function highestBox() {
     var highestBox = 0;
-        $('.list .list_item').each(function(){
-                if($(this).height() > highestBox){
-                highestBox = $(this).height();
-        }
+    $('.list .list_item').each(function(){
+     if ($(this).height() > highestBox ) {
+       highestBox = $(this).height();
+     }
     });
     $('.list .list_item').height(highestBox);
-});
+}
 
+// $(document).ready(function(){
+//
+//     var highestBox = 0;
+//         $('.list .list_item').each(function(){
+//                 if($(this).height() > highestBox){
+//                 highestBox = $(this).height();
+//         }
+//     });
+//     $('.list .list_item').height(highestBox);
+// });
+
+$(document).ready(highestBox);
 $(window).load(highestBox);
 $(window).resize(highestBox);
